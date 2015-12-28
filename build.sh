bc7f7c0749b7efa010d383cd213b331ac0ce7be6
Vladyslav Frolov <frolvlad@salford-systems.com>
2015-12-28 05:33:34 -0800

    Slimmed the image from 819MB to 508MB
    This was done by cleaning up the build dependencies in the same layer as the
    build, and also removing cached packages, apt-lists, and also skipping
    documentation.
    
    Side bonus: added some quotes to the build.sh to make it just more
    bullet-proof.

--- build.sh
+++ build.sh
@@ -4,46 +4,45 @@ set -e
 set -o pipefail
 set -x
 
-
+CPU_COUNT="$(grep -c processor /proc/cpuinfo)"
 BUILD_DIR=/build
 
 
 function install_musl() {
     echo "** Installing musl"
-    cd ${BUILD_DIR}
+    cd "${BUILD_DIR}"
 
     if [ -d "/usr/local/musl" ]; then
         return
     fi
 
-    curl -LO http://www.musl-libc.org/releases/musl-${MUSL_VERSION}.tar.gz
-    tar zxvf musl-${MUSL_VERSION}.tar.gz
-    cd musl-${MUSL_VERSION}
+    curl -L "http://www.musl-libc.org/releases/musl-${MUSL_VERSION}.tar.gz" | tar zxf -
+    cd "musl-${MUSL_VERSION}"
     ./configure
-    make -j4
+    make -j"${CPU_COUNT}"
     make install
 }
 
 function build_llvm_components() {
-    cd ${BUILD_DIR}
+    cd "${BUILD_DIR}"
 
     if [ -f "/usr/local/musl/lib/libunwind.a" ]; then
         return
     fi
 
     echo "** Fetching sources"
-    curl http://llvm.org/releases/${LLVM_VERSION}/llvm-${LLVM_VERSION}.src.tar.xz | tar xJf -
-    curl http://llvm.org/releases/${LLVM_VERSION}/libunwind-${LLVM_VERSION}.src.tar.xz | tar xJf -
+    curl "http://llvm.org/releases/${LLVM_VERSION}/llvm-${LLVM_VERSION}.src.tar.xz" | tar xJf -
+    curl "http://llvm.org/releases/${LLVM_VERSION}/libunwind-${LLVM_VERSION}.src.tar.xz" | tar xJf -
 
-    mv llvm-${LLVM_VERSION}.src llvm
-    mv libunwind-${LLVM_VERSION}.src libunwind
+    mv "llvm-${LLVM_VERSION}.src" llvm
+    mv "libunwind-${LLVM_VERSION}.src" libunwind
 
     echo "** Building libunwind"
 
-    cd ${BUILD_DIR}/libunwind
-    ln -s ${BUILD_DIR}/libcxxabi/include/__cxxabi_config.h ./include/__cxxabi_config.h
+    cd "${BUILD_DIR}/libunwind"
+    ln -s "${BUILD_DIR}/libcxxabi/include/__cxxabi_config.h" ./include/__cxxabi_config.h
     mkdir build && cd build
-    cmake -DLLVM_PATH=${BUILD_DIR}/llvm -DLIBUNWIND_ENABLE_SHARED=OFF ..
+    cmake -DLLVM_PATH="${BUILD_DIR}/llvm" -DLIBUNWIND_ENABLE_SHARED=OFF ..
     make
 
     echo "** Copying to output"
@@ -54,13 +53,14 @@ function build_llvm_components() {
 function build_rust() {
     echo "** Building rust"
 
-    cd ${BUILD_DIR}
+    cd "${BUILD_DIR}"
     if [ ! -d "${BUILD_DIR}/rust" ]; then
-        git clone --depth 1 https://github.com/rust-lang/rust.git
+        git clone --depth 1 "https://github.com/rust-lang/rust.git"
         cd rust
 
-        ./configure                             \
-            --target=x86_64-unknown-linux-musl  \
+        ./configure \
+            --disable-docs \
+            --target=x86_64-unknown-linux-musl \
             --musl-root=/usr/local/musl/
     else
         cd rust
@@ -68,7 +68,7 @@ function build_rust() {
 
     # These environment variables are set from the Dockerfile and control what
     # we build and whether we install the compiler.
-    make ${RUST_BUILD_TARGET}
+    make -j"${CPU_COUNT}" "${RUST_BUILD_TARGET}"
     if [ "x${RUST_BUILD_INSTALL}" == "xtrue" ]; then
         make install
     fi
@@ -81,22 +81,21 @@ function install_cargo() {
         return
     fi
 
-    cd ${BUILD_DIR}
-    curl -LO https://static.rust-lang.org/cargo-dist/cargo-nightly-x86_64-unknown-linux-gnu.tar.gz
-    tar zxf cargo-nightly-x86_64-unknown-linux-gnu.tar.gz
+    cd "${BUILD_DIR}"
+    curl -L "https://static.rust-lang.org/cargo-dist/cargo-nightly-x86_64-unknown-linux-gnu.tar.gz" | tar zxf -
     USER=root ./cargo-nightly-x86_64-unknown-linux-gnu/install.sh
 }
 
 function cleanup() {
     if [ "x${RUST_BUILD_CLEAN}" == "xtrue" ]; then
-        rm -rf ${BUILD_DIR}
+        rm -rf "${BUILD_DIR}"
         apt-get clean
         rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
     fi
 }
 
 function main() {
-    mkdir -p ${BUILD_DIR} && cd ${BUILD_DIR}
+    mkdir -p "${BUILD_DIR}" && cd "${BUILD_DIR}"
 
     install_musl
     build_llvm_components
